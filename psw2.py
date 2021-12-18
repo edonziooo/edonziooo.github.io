7a0c9f4514c66009494d82357e4764030cc0ab4f
Edoardo <81511333+edonziooo@users.noreply.github.com>
2021-12-18 18:49:27 +0100

    e un generatore di password casuali(numeri,lettere)
    ovviamente fatto con python

---
+++ psw2.py
@@ -0,0 +1,22 @@
+# Password generator
+# Author: EDOARDO
+# Version: 1.0
+
+# imports
+import string
+import random
+
+# declaring variables
+password_length = 0
+password = ''
+
+# starting executing
+print('\n\n*** GENERATORE DI PASSWORD CASUALI *** \nBy EDOARDO')
+password_length = input('Scegli la lunghezza della password: ')
+for i in range(int(password_length)):
+    if (random.randint(1, 10) % 2 == 0): 
+        password = password + random.choice(string.ascii_letters)
+    else:
+        password = password + random.choice(string.digits)
+    
+print('\nla tua password generata è: ' + password + '\n\n\n')
